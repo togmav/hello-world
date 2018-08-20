ffe6f2c5bbbd34fbb61303b9d0ef7298f8dadfd8
togmav <tony_griffiths@tony001.c.storied-store-210713.internal>
2018-08-20 18:37:04 +0000

    added first py file

---
+++ test1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#This is a comment
+
+var1="hoo"
+a=5
+b=int(raw_input("Enter a number "))
+
+
+print ("boo" + var1)
+print ("number entered =" + str(b))
+
+for x in range (0,b):
+	print ( var1)
